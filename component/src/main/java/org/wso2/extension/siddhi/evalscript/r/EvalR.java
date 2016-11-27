7834c682d84642bbbab86fd3254853c42a3f2565
Grainier Perera <grainier@wso2.com>
2016-11-27 14:54:20 +0530

    Moving extension from Siddhi Core repository

--- component/src/main/java/org/wso2/extension/siddhi/evalscript/r/EvalR.java
+++ component/src/main/java/org/wso2/extension/siddhi/evalscript/r/EvalR.java
@@ -1,26 +1,30 @@
 /*
- * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c)  2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 
-package org.wso2.gpl.siddhi.extension.evalscript;
+package org.wso2.extension.siddhi.evalscript.r;
 
 
 import org.rosuda.REngine.JRI.JRIEngine;
-import org.rosuda.REngine.*;
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPExpressionVector;
+import org.rosuda.REngine.REXPWrapper;
+import org.rosuda.REngine.REngine;
+import org.rosuda.REngine.RList;
 import org.wso2.siddhi.core.exception.ExecutionPlanCreationException;
 import org.wso2.siddhi.core.exception.ExecutionPlanRuntimeException;
 import org.wso2.siddhi.core.function.EvalScript;
@@ -115,12 +119,12 @@ public class EvalR implements EvalScript {
     }
 
     @Override
-    public void setReturnType(Type returnType) {
-        this.returnType = returnType;
+    public Type getReturnType() {
+        return returnType;
     }
 
     @Override
-    public Type getReturnType() {
-        return returnType;
+    public void setReturnType(Type returnType) {
+        this.returnType = returnType;
     }
 }
--- component/src/test/java/org/wso2/extension/siddhi/evalscript/r/EvalRTestCase.java
+++ component/src/test/java/org/wso2/extension/siddhi/evalscript/r/EvalRTestCase.java
@@ -1,23 +1,22 @@
-
 /*
- * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c)  2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 
-package org.wso2.gpl.siddhi.extension.evalscript;
+package org.wso2.extension.siddhi.evalscript.r;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
@@ -42,7 +41,7 @@ public class EvalRTestCase {
     @Test
     public void testEvalRConcat() throws InterruptedException {
         log.info("TestEvalRConcat");
-        assumeTrue(System.getenv("JRI_HOME")!=null);
+        assumeTrue(System.getenv("JRI_HOME") != null);
 
         SiddhiManager siddhiManager = new SiddhiManager();
 
@@ -84,7 +83,7 @@ public class EvalRTestCase {
     @Test(expected = ExecutionPlanCreationException.class)
     public void testRCompilationFailure() throws InterruptedException {
         log.info("testRCompilationFailure");
-        assumeTrue(System.getenv("JRI_HOME")!=null);
+        assumeTrue(System.getenv("JRI_HOME") != null);
 
         SiddhiManager siddhiManager = new SiddhiManager();
 
