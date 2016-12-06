e4ede04ef07cedf2fad752d2240b08e44164e972
Grainier Perera <grainier@wso2.com>
2016-12-06 16:09:06 +0530

    Fixing groupId

--- component/src/test/java/org/wso2/extension/siddhi/gpl/evalscript/r/EvalRTestCase.java
+++ component/src/test/java/org/wso2/extension/siddhi/gpl/evalscript/r/EvalRTestCase.java
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-package org.wso2.extension.siddhi.evalscript.r;
+package org.wso2.extension.siddhi.gpl.evalscript.r;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
